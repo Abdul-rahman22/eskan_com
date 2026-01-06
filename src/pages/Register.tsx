3b3be5fe583f8d0d1e2de7c6d88e85dbeb2ed788
Abdo7182 <ba2907403@gmail.com>
2026-01-06 23:13:09 +0200

    Update Register.tsx

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -3,8 +3,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-const API_BASE = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000';
-
+const API_BASE = process.env.NEXT_PUBLIC_API_URL || 'https://abdo238923.pythonanywhere.com';
 export default function RegisterPage() {
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
