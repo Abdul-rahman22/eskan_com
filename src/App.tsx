cc8dfeaf17b23df8f5ecec3a84e99f7fc3ab6b66
Abdo7182 <ba2907403@gmail.com>
2026-01-11 20:33:27 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,6 @@ import NotFound from "./pages/NotFound";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
-import AddProperty from "./pages/AddProperty";
-import PropertiesList from "./pages/PropertiesList";
 import { ProtectedRoute } from "./components/ProtectedRoute";
 
 const queryClient = new QueryClient();
@@ -25,41 +23,21 @@ const App = () => (
     <TooltipProvider>
       <BrowserRouter>
         <Routes>
-          {/* Public pages */}
           <Route path="/" element={<Index />} />
           <Route path="/properties" element={<Properties />} />
           <Route path="/property/:id" element={<PropertyDetails />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/admin" element={<Admin />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-
-          {/* Admin */}
-          <Route path="/admin" element={<Admin />} />
-
-          {/* Dashboard */}
           <Route
             path="/dashboard"
             element={<ProtectedRoute element={<Dashboard />} />}
           />
-
-          {/* Add Property */}
-          <Route
-            path="/dashboard/add"
-            element={<ProtectedRoute element={<AddProperty />} />}
-          />
-
-          {/* Properties List */}
-          <Route
-            path="/dashboard/properties"
-            element={<ProtectedRoute element={<PropertiesList />} />}
-          />
-
-          {/* 404 */}
           <Route path="*" element={<NotFound />} />
         </Routes>
 
-        {/* Global UI */}
         <RadixToaster />
         <SonnerToaster />
       </BrowserRouter>
